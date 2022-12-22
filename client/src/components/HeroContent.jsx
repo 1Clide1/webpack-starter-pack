486dc9763b276a1e7cce2379dedf7b3a48202ee7
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-12-21 20:03:38 -0500

    added a super basic header component

---
+++ client/src/components/HeroContent.jsx
@@ -0,0 +1,11 @@
+import '../styles/partials/_hero.scss';
+
+const HeroSection = () => {
+  return (
+    <header className='header'>
+      <h1 className='header-title'>Webpack Starter Pack</h1>
+    </header>
+  );
+};
+
+export default HeroSection;
