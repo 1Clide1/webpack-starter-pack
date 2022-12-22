15aff0a57a7aaba15d1cd6c4610b602a40a590b0
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-12-21 20:01:42 -0500

    fixed a comment typo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,15 @@
 // imports
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const EslintPlugin = require("eslint-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-let mode = "development";
+let mode = 'development';
 // usually target is defaulted to web but can also be node etc setting to web just incase
-let target = "web";
+let target = 'web';
 
 // webpack plugins
 const plugins = [
@@ -17,19 +17,19 @@ const plugins = [
   new MiniCssExtractPlugin(),
   new HtmlWebpackPlugin({
     // my index html file is in the public folder
-    template: path.join(__dirname, "public", "index.html"),
+    template: path.join(__dirname, 'public', 'index.html'),
   }),
   new EslintPlugin(),
   new CompressionPlugin({
-    algorithm: "gzip",
+    algorithm: 'gzip',
   }),
 ];
 
 //  if webpack is in production build
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
   // not sure if the browserslist is actually bugged still but if it ain't broke I will not change this
-  target = "browserslist";
+  target = 'browserslist';
 }
 
 // only want React Hot Reloading in serve mode
@@ -43,13 +43,13 @@ module.exports = {
   mode: mode,
 
   // entry point of my app
-  entry: "./src/index.jsx",
+  entry: './src/index.jsx',
 
   //   setting the ouput of the webpack
   output: {
-    path: path.resolve(__dirname, "./build"),
+    path: path.resolve(__dirname, './build'),
     // this places all images processed in an image folder
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: 'images/[hash][ext][query]',
   },
 
   //   all of the modules have rules and this is how to add said rules
@@ -61,28 +61,28 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             // This is required for asset imports in CSS, such as url()
-            options: { publicPath: "" },
+            options: { publicPath: '' },
           },
-          "css-loader",
-          "postcss-loader",
+          'css-loader',
+          'postcss-loader',
           //   sass needs to be at the bottom to avoid any post css errors
-          "sass-loader",
+          'sass-loader',
         ],
       },
 
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset",
+        type: 'asset',
       },
 
       //   module rules for bable
       {
-        // updated regex because hte previous regex seemed  to not work
+        // updated regex because the previous regex seemed  to not work
         test: /\.(js|jsx|ts|tsx)$/,
         // this is supposed to be regex
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           // needs to be inside use
           options: {
             cacheDirectory: true,
@@ -98,14 +98,14 @@ module.exports = {
 
   //   possible js extensions
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
 
-  devtool: "source-map",
+  devtool: 'source-map',
 
   devServer: {
     // contentbase is doesn't work now use static instead
-    static: "./build",
+    static: './build',
     hot: true,
     port: 3000, // default 8000
     // this should render a browser level overlay if there is a compiler error
